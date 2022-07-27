9594cd2ad24a5ac2968126db28d90835803a906a
GeorgeIsFounded <iqbalnie@gmail.com>
2022-07-27 11:42:22 +0700

    ReactIqbal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// // import logo from './logo.svg';
+// // import './App.css';
+
+// import React from 'react';
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Hello World
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// // function App() {
+// //   return (
+// //     <div>
+// //       <h1>Helloh</h1>
+// //     </div>
+// //   )
+// // }
+
+// function App() {
+//   let a = 20
+//   let b = 10
+//   return (
+//     <React.Fragment>
+//       <h1>Hello World ke-{a}</h1>
+//       <h1>Hello World ke-{a+b}</h1>
+//       <button>button</button>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Header from "./Component/header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return  (
+    <React.Fragment>
+      <h1>Latihan Export Import</h1>
+      <Header/>
+    </React.Fragment>
   );
 }
 
---
+++ src/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Tes from "../src/Component/module/tes"
+
+function Page(){
+    return (
+        <React.Fragment>
+            <h1>Ini Adalah Page</h1>
+            <Tes />
+        </React.Fragment>
+    )
+}
+
+export default Page
