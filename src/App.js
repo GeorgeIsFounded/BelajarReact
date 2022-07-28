60a53440e559c96b20048cf51f67e795c693d9b8
GeorgeIsFounded <iqbalnie@gmail.com>
2022-07-28 14:38:47 +0700

    belajar props bagian 1

--- src/App.js
+++ src/App.js
@@ -49,11 +49,23 @@
 // export default App;
 
 import React from "react";
+import "./styles/styles.css"
+import Identitas from "./module/identitas";
+import Nilai from "./module/nilai";
 
 function App() {
+  let [data, setData] = React.useState([10,20,30,40,50])
   return  (
     <React.Fragment>
-      <h1>Hello</h1>
+      <h1>Latihan Props</h1>
+      <section className="section">
+        <Identitas nama={"Iqbal"} kelas={"XI RPL"} nilai={"97"}/>
+        <Identitas nama={"Fathan"} kelas={"XI RPL"} nilai={"94"}/>
+        <Identitas nama={"Hilmi"} kelas={"XI RPL"} nilai={"91"}/>
+        <Identitas nama={"Alfarabi"} kelas={"XI RPL"} nilai={"99"}/>
+        <Identitas />
+        <Nilai nama={"george"} data={data}/>
+      </section>
     </React.Fragment>
   );
 }
