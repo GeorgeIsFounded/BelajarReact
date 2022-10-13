493aeb3a54d80547e0dc4b375477262ad584f5f0
GeorgeIsFounded <iqbalnie@gmail.com>
2022-10-13 09:47:14 +0700

    gajadi\

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return  (
     <React.Fragment>
-      <h1>Hello</h1>
+      <div className="w-screen h-screen bg-red-500">
+        <div className="bg-blue-200 h-"></div>
+      </div>
     </React.Fragment>
   );
 }
